20cc3af38623e1e2e285f7da3dcffd7607eaaf1b
andrewhoward10 <andrewhoward200@gmail.com>
2021-09-12 20:09:16 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.print("What is the length of your wall in feet? ");
+        int length = keyboardInput.nextInt();
+
+        System.out.print("What is the width of your wall in feet? ");
+        int width = keyboardInput.nextInt();
+
+        int area = length * width;
+
+        int oneGallon = area / 350;
+        if ((area % 350) != 0)
+        {
+            oneGallon++;
+        }
+
+        System.out.print("You will need to purchase " + oneGallon + " gallons of paint to cover " + area + " square feet.");
+
+    }
+}
